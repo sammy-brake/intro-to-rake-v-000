ab10d2a40b33f40325fc376b6ccb346afc45e4a2
Samantha brake <sammy.brake@gmail.com>
2018-11-13 15:28:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,4 +51,16 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  namespace :greeting do
+desc 'outputs hello to the terminal'
+  task :hello do
+    puts "hello from Rake!"
+  end
+ 
+  desc 'outputs hola to the terminal'
+  task :hola do
+    puts "hola de Rake!"
+  end
+end
+
 end
